cbd7abff03e1fbe986525f960fa1c559b6c837fd
Angie Pineda <agpinedam@unal.edu.co>
2021-08-23 12:39:58 -0500

    Operations done, results don't saved yet

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -6,10 +6,9 @@ public class Main {
 
     public static void main(String[] args) {
         boolean continueOperations = false;
-        HashMap<String,String> valuesAndOperations= new HashMap<>();
+        HashMap<String,String> valuesAndOperations;
         HashMap<String,Integer> results = new HashMap<>();
         results.put("a",1);
-        String validOperation;
 
         while (!continueOperations) {
             System.out.println("Ingrese el comando");
@@ -17,30 +16,8 @@ public class Main {
             Values values = new Values(input.nextLine());
             values.isValidOperation();
             valuesAndOperations = values.getValues();
-            validOperation= valuesAndOperations.get("operation");
-            Operations operation = new Operations();
-                switch (validOperation){
-                    case "Exit":
-                        continueOperations=true;
-                    case "Show":
-                        operation.Show(valuesAndOperations.get("x"),results);
-                        break;
-                    default:
-                        GetXY intX = new GetXY(valuesAndOperations,results,"x");
-                        GetXY intY = new GetXY(valuesAndOperations,results,"y");
-                        if(intX.isContinueProcess() && intY.isContinueProcess()){
-                            int x = intX.getValue();
-                            int y = intY.getValue();
-                            System.out.println("El valor de x es "+ x);
-                            System.out.println("El valor de y es "+ y);
-                            operation.Minus(x,y,"a");
-                            continueOperations = false;
-                        }else{
-                            continueOperations = false;
-                            System.out.println("Please ingres valid values");
-                        }
-                        break;
-                }
+            Operations operation = new Operations(valuesAndOperations.get("operation"));
+            continueOperations = operation.choseOperation(valuesAndOperations,results);
         }
     }
 }
--- src/com/company/Operations.java
+++ src/com/company/Operations.java
@@ -3,17 +3,60 @@ package com.company;
 import java.util.HashMap;
 
 public class Operations {
+    private final String operation;
 
-    public void Show(String variable, HashMap<String,Integer> results){
+    public Operations(String operation){
+        this.operation=operation;
+    }
+
+    public boolean choseOperation(HashMap<String,String>valuesAndOperations,HashMap<String,Integer>results){
+        boolean exit=false;
+        switch (operation) {
+            case "Exit" -> exit = true;
+            case "Show" -> Show(valuesAndOperations.get("x"), results);
+            default -> {
+                GetXY intX = new GetXY(valuesAndOperations, results, "x");
+                GetXY intY = new GetXY(valuesAndOperations, results, "y");
+                if (intX.isContinueProcess() && intY.isContinueProcess()) {
+                    int x = intX.getValue();
+                    int y = intY.getValue();
+                    calculate(operation, x, y);
+                } else System.out.println("Please ingres valid values");
+            }
+        }
+        return exit;
+    }
+
+    private void calculate(String operation, int x, int y){
+        switch (operation){
+            case "Sum"-> Sum(x,y);
+            case "Minus" -> Minus(x,y);
+            case "Multiply"-> Multiply(x,y);
+            case "Divide"-> Divide(x,y);
+        }
+    }
+
+    private void Show(String variable, HashMap<String,Integer> results){
         if (!results.containsKey(variable)){
             System.out.println("The variable "+variable+" was not saved yet");
         }else {
             System.out.println("The variable "+ variable+" has the value of "+ results.get(variable));
         }
     }
-    public void Minus(int x, int y, String result){
+
+    private void Sum(int x, int y){
+        System.out.println("Result is "+ (x+y) );
+    }
+
+    private void Minus(int x, int y){
         System.out.println("Result is "+ (x-y) );
     }
 
+    private void Multiply(int x, int y){
+        System.out.println("Result is "+ (x*y) );
+    }
+    private void Divide(int x, int y){
+        System.out.println("Result is "+ (x/y) );
+    }
 
 }
